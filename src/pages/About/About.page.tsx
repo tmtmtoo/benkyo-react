0d5d07ae695c0f9059a64a0cea130723d46e2149
tmtmtoo <tmtmt2209@gmail.com>
2022-10-09 00:28:46 +0900

    feat: link to github repository

--- src/pages/About/About.page.tsx
+++ src/pages/About/About.page.tsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+
 export const AboutPage = () => (
-  <main>React を勉強している人の練習リポジトリだ</main>
+  <main>
+    <p>React を勉強している人の練習リポジトリです</p>
+    <p>
+      <a
+        href="https://github.com/tmtmtoo/benkyo-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        tmtmtoo/benkyo-react
+      </a>
+    </p>
+    <Link to="/">戻る</Link>
+  </main>
 );
