c9329531d1b5e3ea7c651fcbacf43aa297d4d100
tmtmtoo <tmtmt2209@gmail.com>
2022-10-12 19:42:21 +0900

    chore: update about page

--- src/components/pages/About/About.tsx
+++ src/components/pages/About/About.tsx
@@ -1,5 +1,7 @@
 import { Page } from "../../ui/Page";
 import { Markdown } from "../../ui/Markdown";
+import logo from "./logo.png";
+import styled from "styled-components";
 
 const CONTENT = `
 ## このページについて
@@ -7,8 +9,15 @@ const CONTENT = `
 React を勉強している人の備忘録です。
 `;
 
+const Author = styled.img`
+  margin-top: 20px;
+  width: 150px;
+  height: 150px;
+`;
+
 export const About = () => (
   <Page showFooter>
     <Markdown content={CONTENT} />
+    <Author src={logo} />
   </Page>
 );
